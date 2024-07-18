b0eca54610c85369817db6cbd764c20b97ec549a
DAndyPROG <domikandrew123@gmail.com>
2024-07-18 18:33:04 +0300

    Added setup and queries scripts

--- queries.sql
+++ queries.sql
@@ -25,4 +25,22 @@ SELECT * FROM customers WHERE email LIKE '%@gmail.com';
 SELECT COUNT(*) FROM orders;
 
 SELECT COUNT(*) FROM orders WHERE customer_id = 1;  
+
 SELECT * FROM products WHERE price BETWEEN 10 AND 100;
+
+SELECT * FROM products WHERE price > 500;
+
+SELECT * FROM orders WHERE order_date > '2024-01-01';
+
+SELECT * FROM orders WHERE quantity > 1;
+
+SELECT * FROM suppliers WHERE contact_name = 'Vlad Ushakov';
+
+SELECT * FROM products ORDER BY price DESC LIMIT 1;
+
+SELECT AVG(price) AS average_price FROM products;
+
+SELECT SUM(p.price * o.quantity) AS total_amount
+FROM orders o
+JOIN products p ON o.product_id = p.id
+WHERE o.customer_id = 1;
